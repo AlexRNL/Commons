eeb40831ffd86d4fb17d333e7d0bc67d6a92f8ee
Alex Barféty <alex.barfety@free.fr>
2013-03-09 00:31:17 +0100

    Complete javadoc and don't throw NPE

--- src/main/java/com/alexrnl/commons/error/ExceptionUtils.java
+++ src/main/java/com/alexrnl/commons/error/ExceptionUtils.java
@@ -1,10 +1,9 @@
 package com.alexrnl.commons.error;
 
-import java.util.Objects;
 import java.util.logging.Logger;
 
 /**
- * TODO
+ * Utility methods for exception handling.<br />
  * @author Alex
  */
 public final class ExceptionUtils {
@@ -20,15 +19,17 @@ public final class ExceptionUtils {
 	}
 	
 	/**
-	 * 
+	 * Print a nice message with the {@link Class} and the {@link Throwable#getMessage() message}
+	 * of the exception.<br />
 	 * @param e
 	 *        the throwable object to display.
 	 * @return a {@link String} containing the class and the message of the exception.
-	 * @throws NullPointerException
-	 *         if the {@link Throwable} is <code>null</code>.
 	 */
-	public static String displayClassAndMessage (final Throwable e) throws NullPointerException {
-		Objects.requireNonNull(e);
+	public static String displayClassAndMessage (final Throwable e) {
+		if (e == null) {
+			lg.warning("Cannot display null exception.");
+			return "null exception caught";
+		}
 		return e.getClass() + "; " + e.getMessage();
 	}
 }
