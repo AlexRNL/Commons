beef11a90f643d5cedea4083d6e21cc69330667a
Alex Barféty <alexbarfety@free.fr>
2013-03-16 16:42:06 +0100

    Format javadoc

--- src/main/java/com/alexrnl/commons/time/Time.java
+++ src/main/java/com/alexrnl/commons/time/Time.java
@@ -77,7 +77,8 @@ public class Time implements Serializable, Comparable<Time> {
 	
 	/**
 	 * Constructor #4.<br />
-	 * @param timeStamp the number of seconds since January 1st, 1970.
+	 * @param timeStamp
+	 *        the number of seconds since January 1st, 1970.
 	 */
 	public Time (final long timeStamp) {
 		this(new Date(timeStamp));
