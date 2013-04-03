9e823bdb367e4c9bbf0fc5b9c1a32b8c8c1337f7
Alex Barféty <alexbarfety@free.fr>
2013-04-04 00:09:55 +0200

    The Configuration class is not a singleton anymore.
    Several Configuration instances can be used simultaneously in an
    application.

--- src/main/java/com/alexrnl/commons/utils/Configuration.java
+++ src/main/java/com/alexrnl/commons/utils/Configuration.java
@@ -14,41 +14,44 @@ import com.alexrnl.commons.error.ExceptionUtils;
  * Class access the application configuration.<br />
  * To retrieve a property, the method {@link #get(String)} should be used.
  * The default configuration file used will be <code>conf/configuration.xml</code>, it may be
- * changed using the {@link #setFile(String)} method. To avoid any warning, use this method before
- * calling any {@link #getInstance()}.
+ * changed using the {@link #setFile(String)} method.<br />
  * The {@link Configuration} is thread-safe as it is based on the {@link Properties} class, itself
  * extending a {@link Hashtable}.
+ * Load the specified file using the {@link #load()} method. If the object is not loaded, it will be
+ * loaded on the first call to the {@link #get(String)} method (which, thus may take a bit of time).
  * @author Alex
  */
 public final class Configuration {
 	/** Logger */
-	private static Logger			lg					= Logger.getLogger(Configuration.class.getName());
+	private static Logger		lg							= Logger.getLogger(Configuration.class.getName());
 	
-	/** Unique instance of the class */
-	private static Configuration	singleton			= new Configuration();
+	/** The default configuration file to load. */
+	private static String		DEFAULT_CONFIGURATION_FILE	= "conf/configuration.xml";
+	
 	/** The configuration file to load */
-	private static String			configurationFile	= "conf/configuration.xml";
-	
+	private String				configurationFile;
 	/** The configuration properties */
-	private final Properties		configuration;
+	private final Properties	configuration;
+	/** The state of the configuration, <code>true</code> if loaded */
+	private boolean				loaded;
 	
 	/**
 	 * Constructor #1.<br />
 	 * Private default constructor, load the configuration properties from the configuration file.
 	 */
-	private Configuration () {
-		super();
-		configuration = new Properties();
-		load();
+	public Configuration () {
+		this(DEFAULT_CONFIGURATION_FILE);
 	}
 	
 	/**
-	 * Return the unique instance of the singleton.<br />
-	 * Build the properties which will load the configuration.
-	 * @return the instance of the configuration.
+	 * Constructor #2.<br />
+	 * @param configurationFile the configuration file to load.
 	 */
-	public static Configuration getInstance () {
-		return singleton;
+	public Configuration (final String configurationFile) {
+		super();
+		this.configurationFile = configurationFile;
+		configuration = new Properties();
+		loaded = false;
 	}
 	
 	/**
@@ -56,25 +59,22 @@ public final class Configuration {
 	 * @param filePath
 	 *        the path to the configuration file to load.
 	 */
-	public static void setFile (final String filePath) {
+	public void setFile (final String filePath) {
 		configurationFile = filePath;
-		if (singleton == null) {
-			getInstance();
-		} else {
-			getInstance().load();
-		}
+		loaded = false;
+		load();
 	}
 	
 	/**
 	 * Load the properties from the configuration file.
 	 */
-	private void load () {
-		// Load the properties
+	public void load () {
 		try {
 			configuration.clear();
 			configuration.loadFromXML(new FileInputStream(configurationFile));
+			loaded = true;
 			if (lg.isLoggable(Level.INFO)) {
-				lg.info("Properties loaded from file: " + configurationFile);
+				lg.info(size() + "Properties loaded from file: " + configurationFile);
 			}
 			if (lg.isLoggable(Level.FINE)) {
 				lg.fine("Properties successfully loaded:");
@@ -94,6 +94,9 @@ public final class Configuration {
 	 * @return the value of the property.
 	 */
 	public String get (final String propertyName) {
+		if (!loaded) {
+			load();
+		}
 		final String propertyValue = configuration.getProperty(propertyName);
 		if (propertyValue != null) {
 			lg.warning("No property with name " + propertyName);
@@ -103,10 +106,19 @@ public final class Configuration {
 	}
 	
 	/**
+	 * Check if the configuration file is loaded.
+	 * @return <code>true</code> if the configuration is loaded.
+	 */
+	public boolean isLoaded () {
+		return loaded;
+	}
+	
+	/**
 	 * Return the number of properties held by the configuration file.
 	 * @return the number of properties.
 	 */
 	public int size () {
 		return configuration.size();
 	}
+	
 }
