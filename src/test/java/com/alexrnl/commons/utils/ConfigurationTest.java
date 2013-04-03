22422cd816535619acf9eb1840cd8aa883fa0a0a
Alex Barféty <alexbarfety@free.fr>
2013-04-03 23:04:06 +0200

    Add test suite for the Configuration class

---
+++ src/test/java/com/alexrnl/commons/utils/ConfigurationTest.java
@@ -0,0 +1,76 @@
+package com.alexrnl.commons.utils;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Test suite for the {@link Configuration} class.
+ * @author Alex
+ */
+public class ConfigurationTest {
+	
+	/**
+	 * 
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass () {
+	}
+	
+	/**
+	 * 
+	 */
+	@AfterClass
+	public static void tearDownAfterClass () {
+	}
+	
+	/**
+	 * 
+	 */
+	@Before
+	public void setUp () {
+	}
+	
+	/**
+	 * 
+	 */
+	@After
+	public void tearDown () {
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.Configuration#getInstance()}.
+	 */
+	@Test
+	public void testGetInstance () {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.Configuration#setFile(java.lang.String)}.
+	 */
+	@Test
+	public void testSetFile () {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.Configuration#get(java.lang.String)}.
+	 */
+	@Test
+	public void testGet () {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.Configuration#size()}.
+	 */
+	@Test
+	public void testSize () {
+		fail("Not yet implemented"); // TODO
+	}
+}
