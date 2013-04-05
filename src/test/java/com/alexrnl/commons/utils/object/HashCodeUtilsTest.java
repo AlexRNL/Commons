5c7941d61a2d2da11dfca56f1d7f8de41bb3506f
Alex Barféty <alexbarfety@free.fr>
2013-04-05 22:44:03 +0200

    Test suite for the HashCodeUtils class

---
+++ src/test/java/com/alexrnl/commons/utils/object/HashCodeUtilsTest.java
@@ -0,0 +1,68 @@
+package com.alexrnl.commons.utils.object;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test suite for the {@link HashCodeUtils} class.
+ * @author Alex
+ */
+public class HashCodeUtilsTest {
+	
+	/**
+	 * Set up attributes.
+	 */
+	@Before
+	public void setUp () {
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.object.HashCodeUtils#hashCode(int, int, java.lang.Iterable)}.
+	 */
+	@Test
+	public void testHashCodeIntIntIterableOfObject () {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.object.HashCodeUtils#hashCode(int, int, java.lang.Object[])}.
+	 */
+	@Test
+	public void testHashCodeIntIntObjectArray () {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.object.HashCodeUtils#hashCode(int, java.lang.Iterable)}.
+	 */
+	@Test
+	public void testHashCodeIntIterableOfObject () {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.object.HashCodeUtils#hashCode(int, java.lang.Object[])}.
+	 */
+	@Test
+	public void testHashCodeIntObjectArray () {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.object.HashCodeUtils#hashCode(java.lang.Iterable)}.
+	 */
+	@Test
+	public void testHashCodeIterableOfObject () {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.utils.object.HashCodeUtils#hashCode(java.lang.Object[])}.
+	 */
+	@Test
+	public void testHashCodeObjectArray () {
+		fail("Not yet implemented"); // TODO
+	}
+}
--- src/test/java/com/alexrnl/commons/utils/object/ObjectTests.java
+++ src/test/java/com/alexrnl/commons/utils/object/ObjectTests.java
@@ -9,6 +9,6 @@ import org.junit.runners.Suite.SuiteClasses;
  * @author Alex
  */
 @RunWith(Suite.class)
-@SuiteClasses({ AttributeComparatorTest.class, AutoCompareTest.class })
+@SuiteClasses({ AttributeComparatorTest.class, AutoCompareTest.class, HashCodeUtilsTest.class })
 public class ObjectTests {
 }
