f3f163e87255b07fb11a1ea042a5fb023a69ce36
Alex Barféty <alexbarfety@free.fr>
2013-04-07 23:31:23 +0200

    Split class loading and instantiation in two methods
    Also, fix the Javadoc.

--- src/main/java/com/alexrnl/commons/database/AbstractDAOFactory.java
+++ src/main/java/com/alexrnl/commons/database/AbstractDAOFactory.java
@@ -9,7 +9,7 @@ import com.alexrnl.commons.error.ExceptionUtils;
 /**
  * The abstract factory for the {@link DAO}s.<br />
  * It manages the creation of the factory through the {@link #createFactory(String)} method.<br />
- * The user of this class should extend id and add the required {@link DAO} methods needed for its
+ * The user of this class should extend it and add the required {@link DAO} methods needed for its
  * needs.
  * @author Alex
  */
@@ -29,11 +29,11 @@ public abstract class AbstractDAOFactory implements Closeable {
 	}
 	
 	/**
-	 * Retrieve and create the appropriate factory.
+	 * Create the factory from the class specified.
 	 * @param factoryClass
 	 *        the type of DAO required.
 	 */
-	public static synchronized void createFactory (final String factoryClass) {
+	public static synchronized void createFactory (final Class<? extends AbstractDAOFactory> factoryClass) {
 		try {
 			if (implementation != null) {
 				try {
@@ -42,8 +42,22 @@ public abstract class AbstractDAOFactory implements Closeable {
 					lg.warning("Could not close previous implementation (" + e.getMessage() + ")");
 				}
 			}
-			implementation = Class.forName(factoryClass).asSubclass(AbstractDAOFactory.class).newInstance();
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | ClassCastException e) {
+			implementation = factoryClass.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			lg.severe("Cannot instantiate DAO factory class (" + factoryClass + "). " + ExceptionUtils.display(e));
+			throw new DAOInstantiationError(factoryClass.getName(), e);
+		}
+	}
+	
+	/**
+	 * Retrieve and create the appropriate factory.
+	 * @param factoryClass
+	 *        the type of DAO required.
+	 */
+	public static void createFactory (final String factoryClass) {
+		try {
+			createFactory(Class.forName(factoryClass).asSubclass(AbstractDAOFactory.class));
+		} catch (ClassNotFoundException | ClassCastException e) {
 			lg.severe("Cannot instantiate DAO factory class (" + factoryClass + "). " + ExceptionUtils.display(e));
 			throw new DAOInstantiationError(factoryClass, e);
 		}
