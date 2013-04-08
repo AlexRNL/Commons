54ba7598f0d50e5c52784548d1adcfa5e1879fdc
Alex Barféty <alexbarfety@free.fr>
2013-04-08 23:46:00 +0200

    Update test so that the Dummy class check the doability of the map
    Following commit a6a541098a94f634d19fa6bd620ad87132292ae4

--- src/test/java/com/alexrnl/commons/database/NoIdErrorTest.java
+++ src/test/java/com/alexrnl/commons/database/NoIdErrorTest.java
@@ -2,6 +2,7 @@ package com.alexrnl.commons.database;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.EnumMap;
 import java.util.Map;
 
 import org.junit.Test;
@@ -20,17 +21,39 @@ public class NoIdErrorTest {
 		/** Serial version UID */
 		private static final long	serialVersionUID	= 1L;
 		
+		/** Map with the columns */
+		private EnumMap<DummyColumn, Column> map;
+		
 		@Override
 		public String getEntityName () {
 			return "Dummy";
 		}
 		
-		@Override
-		protected void setEntityColumns () {}
+		/** Dummy columns enumeration */
+		public enum DummyColumn implements EntityColumn {
+			/** The id column */
+			ID,
+			/** The name column */
+			NAME;
+			
+			@Override
+			public String getFieldName () {
+				return this.name();
+			}
+			
+		}
 		
 		@Override
-		public Map<? extends Enum<?>, Column> getEntityColumns () {
-			return null;
+		protected void setEntityColumns () {
+			// No synchronization for the tests
+			map = new EnumMap<>(DummyColumn.class);
+			map.put(DummyColumn.ID, new Column(Integer.class, "id", true));
+			map.put(DummyColumn.NAME, new Column(String.class, "name"));
+		}
+		
+		@Override
+		public Map<? extends Enum<? extends EntityColumn>, Column> getEntityColumns () {
+			return map;
 		}
 		
 		@Override
