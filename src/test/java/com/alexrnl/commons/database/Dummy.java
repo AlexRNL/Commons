0a3a329bf5a5b6de06f82232e0b9530ab56de2a3
Alex Barféty <alexbarfety@free.fr>
2013-04-10 02:27:05 +0200

    Move Dummy class to new file, will be used for tests purposes.

---
+++ src/test/java/com/alexrnl/commons/database/Dummy.java
@@ -0,0 +1,105 @@
+package com.alexrnl.commons.database;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import com.alexrnl.commons.utils.StringUtils;
+
+/**
+ * Dummy entity for test purposes.
+ * @author Alex
+ */
+public class Dummy extends Entity {
+	/** Serial version UID */
+	private static final long	serialVersionUID	= 1L;
+	
+	/** Map with the columns */
+	private EnumMap<Dummy.DummyColumn, Column> map;
+	/** Id of the class */
+	private Integer id;
+	/** Name of the dummy */
+	private String name;
+	
+	@Override
+	public String getEntityName () {
+		return "Dummy";
+	}
+	
+	/** Dummy columns enumeration */
+	public enum DummyColumn implements EntityColumn {
+		/** The id column */
+		ID ("Id"),
+		/** The name column */
+		NAME ("Name");
+		
+		/** The name of the attribute */
+		private final String attributeName;
+		
+		/**
+		 * Constructor #1.<br />
+		 * Unique constructor.
+		 * @param attributeName
+		 *        the name of the attribute.
+		 */
+		private DummyColumn (final String attributeName) {
+			this.attributeName = attributeName;
+		}
+		
+		@Override
+		public String getFieldName () {
+			return attributeName;
+		}
+		
+	}
+	
+	@Override
+	protected void setEntityColumns () {
+		// No synchronization for the tests
+		map = new EnumMap<>(Dummy.DummyColumn.class);
+		map.put(DummyColumn.ID, new Column(Integer.class, "id", true));
+		map.put(DummyColumn.NAME, new Column(String.class, "name"));
+	}
+	
+	@Override
+	public Map<? extends Enum<? extends EntityColumn>, Column> getEntityColumns () {
+		return map;
+	}
+	
+	@Override
+	public String getID () {
+		return StringUtils.emptyIfNull(getId());
+	}
+	
+	/**
+	 * Return the attribute id.
+	 * @return the attribute id.
+	 */
+	public Integer getId () {
+		return id;
+	}
+	
+	/**
+	 * Set the attribute id.
+	 * @param id the attribute id.
+	 */
+	public void setId (final Integer id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Return the attribute name.
+	 * @return the attribute name.
+	 */
+	public String getName () {
+		return name;
+	}
+	
+	/**
+	 * Set the attribute name.
+	 * @param name the attribute name.
+	 */
+	public void setName (final String name) {
+		this.name = name;
+	}
+	
+}
--- src/test/java/com/alexrnl/commons/database/NoIdErrorTest.java
+++ src/test/java/com/alexrnl/commons/database/NoIdErrorTest.java
@@ -2,8 +2,6 @@ package com.alexrnl.commons.database;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.EnumMap;
-import java.util.Map;
 
 import org.junit.Test;
 
@@ -13,55 +11,6 @@ import org.junit.Test;
  */
 public class NoIdErrorTest {
 	
-	/**
-	 * Dummy entity for test purposes.
-	 * @author Alex
-	 */
-	private static class Dummy extends Entity {
-		/** Serial version UID */
-		private static final long	serialVersionUID	= 1L;
-		
-		/** Map with the columns */
-		private EnumMap<DummyColumn, Column> map;
-		
-		@Override
-		public String getEntityName () {
-			return "Dummy";
-		}
-		
-		/** Dummy columns enumeration */
-		public enum DummyColumn implements EntityColumn {
-			/** The id column */
-			ID,
-			/** The name column */
-			NAME;
-			
-			@Override
-			public String getFieldName () {
-				return this.name();
-			}
-			
-		}
-		
-		@Override
-		protected void setEntityColumns () {
-			// No synchronization for the tests
-			map = new EnumMap<>(DummyColumn.class);
-			map.put(DummyColumn.ID, new Column(Integer.class, "id", true));
-			map.put(DummyColumn.NAME, new Column(String.class, "name"));
-		}
-		
-		@Override
-		public Map<? extends Enum<? extends EntityColumn>, Column> getEntityColumns () {
-			return map;
-		}
-		
-		@Override
-		public String getID () {
-			return null;
-		}
-		
-	}
 	/** Exception with no entity information */
 	private final NoIdError	unknownException = new NoIdError();
 	/** Exception with the entity that generated the problem */
