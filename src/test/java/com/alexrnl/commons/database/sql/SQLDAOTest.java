3255663ce12c11c6204bb99c66b006f45d76f114
Alex Barféty <alexbarfety@free.fr>
2013-05-19 01:43:58 +0200

    Implement test for the SQLDAO class
    Using the Dummy entity for test purposes.

--- src/test/java/com/alexrnl/commons/database/sql/SQLDAOTest.java
+++ src/test/java/com/alexrnl/commons/database/sql/SQLDAOTest.java
@@ -1,6 +1,20 @@
 package com.alexrnl.commons.database.sql;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,94 +22,67 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.alexrnl.commons.database.Dummy;
+import com.alexrnl.commons.database.Dummy.DummyColumn;
+
 /**
  * Test suite for the {@link SQLDAO} class.
  * @author Alex
  */
 public class SQLDAOTest {
-	
+	/** The test database connection */
+	private static Connection	connection;
+	/** The DAO for the {@link Dummy} class */
+	private DummySQLDAO			dummyDAO;
+
 	/**
-	 * @throws java.lang.Exception
+	 * Create the embedded database and set-up the dummy table.
+	 * @throws Exception
+	 *         if there was a problem while initializing the database.
 	 */
 	@BeforeClass
 	public static void setUpBeforeClass () throws Exception {
+		connection = DriverManager.getConnection("jdbc:h2:mem:");
+		final PreparedStatement createTable = connection.prepareStatement("CREATE TABLE dummy ("
+				+ "id		INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+				+ "name		VARCHAR_IGNORECASE(60) NOT NULL);");
+		createTable.executeUpdate();
 	}
 	
 	/**
-	 * @throws java.lang.Exception
+	 * Close the database connection, after all tests have been carried out.
+	 * @throws Exception if there was a problem while closing the database connection.
 	 */
 	@AfterClass
 	public static void tearDownAfterClass () throws Exception {
+		connection.close();
 	}
 	
 	/**
-	 * @throws java.lang.Exception
+	 * Set up test attributes.
+	 * @throws Exception
+	 *         if there was a problem while initializing the DAO.
 	 */
 	@Before
 	public void setUp () throws Exception {
+		Logger.getLogger(SQLDAO.class.getName()).setLevel(Level.FINER);
+		dummyDAO = new DummySQLDAO(connection);
 	}
 	
 	/**
-	 * @throws java.lang.Exception
+	 * Close the DAO after test use.<br />
+	 * @throws Exception
+	 *         if there was an issue while closing the DAO.
 	 */
 	@After
 	public void tearDown () throws Exception {
-	}
-	
-	/**
-	 * Test method for {@link com.alexrnl.commons.database.sql.SQLDAO#SQLDAO(java.sql.Connection)}.
-	 */
-	@Test
-	public void testSQLDAO () {
-		fail("Not yet implemented"); // TODO
-	}
-	
-	/**
-	 * Test method for {@link com.alexrnl.commons.database.sql.SQLDAO#getConnection()}.
-	 */
-	@Test
-	public void testGetConnection () {
-		fail("Not yet implemented"); // TODO
-	}
-	
-	/**
-	 * Test method for {@link com.alexrnl.commons.database.sql.SQLDAO#getEntitySample()}.
-	 */
-	@Test
-	public void testGetEntitySample () {
-		fail("Not yet implemented"); // TODO
-	}
-	
-	/**
-	 * Test method for {@link com.alexrnl.commons.database.sql.SQLDAO#createEntityFromResult(java.sql.ResultSet)}.
-	 */
-	@Test
-	public void testCreateEntityFromResult () {
-		fail("Not yet implemented"); // TODO
-	}
-	
-	/**
-	 * Test method for {@link com.alexrnl.commons.database.sql.SQLDAO#fillInsertStatement(java.sql.PreparedStatement, com.alexrnl.commons.database.Entity)}.
-	 */
-	@Test
-	public void testFillInsertStatement () {
-		fail("Not yet implemented"); // TODO
-	}
-	
-	/**
-	 * Test method for {@link com.alexrnl.commons.database.sql.SQLDAO#fillUpdateStatement(java.sql.PreparedStatement, com.alexrnl.commons.database.Entity)}.
-	 */
-	@Test
-	public void testFillUpdateStatement () {
-		fail("Not yet implemented"); // TODO
-	}
-	
-	/**
-	 * Test method for {@link com.alexrnl.commons.database.sql.SQLDAO#close()}.
-	 */
-	@Test
-	public void testClose () {
-		fail("Not yet implemented"); // TODO
+		for (final Dummy dummy : dummyDAO.retrieveAll()) {
+			if (!dummyDAO.delete(dummy)) {
+				throw new SQLException("Could not properly delete " + dummy);
+			}
+		}
+		dummyDAO.close();
+		
 	}
 	
 	/**
@@ -103,7 +90,17 @@ public class SQLDAOTest {
 	 */
 	@Test
 	public void testCreate () {
-		fail("Not yet implemented"); // TODO
+		assertEquals(0, dummyDAO.retrieveAll().size());
+		final Dummy dummy = new Dummy("test");
+		final Dummy newDummy = dummyDAO.create(dummy);
+		
+		assertNotNull(newDummy);
+		assertEquals(1, dummyDAO.retrieveAll().size());
+		// Objects are not equals because the id is not set on the first dummy
+		assertNotEquals(dummy, newDummy);
+		assertEquals("test", newDummy.getName());
+		
+		assertNull(dummyDAO.create(null));
 	}
 	
 	/**
@@ -111,7 +108,12 @@ public class SQLDAOTest {
 	 */
 	@Test
 	public void testFind () {
-		fail("Not yet implemented"); // TODO
+		assertEquals(0, dummyDAO.retrieveAll().size());
+		final Dummy aba = dummyDAO.create(new Dummy("ABA"));
+		final Dummy ldr = dummyDAO.create(new Dummy("LDR"));
+		
+		assertEquals(aba, dummyDAO.find(aba.getId()));
+		assertEquals(ldr, dummyDAO.find(ldr.getId()));
 	}
 	
 	/**
@@ -119,7 +121,15 @@ public class SQLDAOTest {
 	 */
 	@Test
 	public void testUpdate () {
-		fail("Not yet implemented"); // TODO
+		assertEquals(0, dummyDAO.retrieveAll().size());
+		final Dummy aba = dummyDAO.create(new Dummy("ABA"));
+		aba.setName("aba");
+		assertTrue(dummyDAO.update(aba));
+		final Dummy updated = dummyDAO.find(aba.getId());
+		assertEquals(aba, updated);
+		assertEquals("aba", updated.getName());
+		
+		assertFalse(dummyDAO.update(null));
 	}
 	
 	/**
@@ -127,7 +137,14 @@ public class SQLDAOTest {
 	 */
 	@Test
 	public void testDelete () {
-		fail("Not yet implemented"); // TODO
+		assertEquals(0, dummyDAO.retrieveAll().size());
+		final Dummy aba = dummyDAO.create(new Dummy("ABA"));
+		assertEquals(1, dummyDAO.retrieveAll().size());
+		dummyDAO.delete(aba);
+		assertEquals(0, dummyDAO.retrieveAll().size());
+		assertNull(dummyDAO.find(aba.getId()));
+		
+		assertTrue(dummyDAO.delete(null));
 	}
 	
 	/**
@@ -135,7 +152,17 @@ public class SQLDAOTest {
 	 */
 	@Test
 	public void testRetrieveAll () {
-		fail("Not yet implemented"); // TODO
+		assertEquals(0, dummyDAO.retrieveAll().size());
+		dummyDAO.create(new Dummy("ABA"));
+		dummyDAO.create(new Dummy("LDR"));
+		
+		final Set<Dummy> dummies = dummyDAO.retrieveAll();
+		assertEquals(2, dummies.size());
+		for (final Dummy dummy : dummies) {
+			if (!dummy.getName().equals("ABA") && !dummy.getName().equals("LDR")) {
+				fail("Unknwon dummy found: " + dummy);
+			}
+		}
 	}
 	
 	/**
@@ -143,6 +170,21 @@ public class SQLDAOTest {
 	 */
 	@Test
 	public void testSearch () {
-		fail("Not yet implemented"); // TODO
+		// Populate table
+		dummyDAO.create(new Dummy("ABA"));
+		dummyDAO.create(new Dummy("LDR"));
+		dummyDAO.create(new Dummy("123"));
+		dummyDAO.create(new Dummy("A2E"));
+		dummyDAO.create(new Dummy("AXE"));
+		dummyDAO.create(new Dummy("BLO"));
+		dummyDAO.create(new Dummy("LUD"));
+		
+		final Set<Dummy> allDummies = dummyDAO.retrieveAll();
+		assertEquals(allDummies, dummyDAO.search(null, "test"));
+		assertEquals(allDummies, dummyDAO.search(Dummy.getColumns().get(DummyColumn.NAME), null));
+
+		assertEquals(3, dummyDAO.search(Dummy.getColumns().get(DummyColumn.NAME), "A%").size());
+		assertEquals(1, dummyDAO.search(Dummy.getColumns().get(DummyColumn.NAME), "ABA").size());
+		assertEquals(2, dummyDAO.search(Dummy.getColumns().get(DummyColumn.NAME), "%2%").size());
 	}
 }
