0cac8dfdeb0bc8d07b3225542a5fb36e9fef56c5
Alex Barféty <alexbarfety@free.fr>
2013-07-17 00:22:35 +0200

    Add the interface for a parametrable translation

---
+++ src/main/java/com/alexrnl/commons/translation/ParametrableTranslation.java
@@ -0,0 +1,16 @@
+package com.alexrnl.commons.translation;
+
+/**
+ * Interface for configurable translations keys.<br />
+ * Classes defining translations for a given dialog, title, label can be parameterize easily using
+ * this interface.
+ * @author Alex
+ */
+public interface ParametrableTranslation {
+	
+	/**
+	 * Return the parameters to use when building the translation of the key.
+	 * @return the parameters to use.
+	 */
+	Object[] getParameters ();
+}
