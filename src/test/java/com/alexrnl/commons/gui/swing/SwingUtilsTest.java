c8957648d1d2aae3b260086e490fe706163a079b
Alex Barféty <alexbarfety@free.fr>
2013-07-20 23:17:22 +0200

    Implement test for the SwingUtils.getMenu method

--- src/test/java/com/alexrnl/commons/gui/swing/SwingUtilsTest.java
+++ src/test/java/com/alexrnl/commons/gui/swing/SwingUtilsTest.java
@@ -1,21 +1,40 @@
 package com.alexrnl.commons.gui.swing;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.JMenu;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.alexrnl.commons.translation.Translator;
 
 /**
  * Test suite for the {@link SwingUtils} class.
  * @author Alex
  */
 public class SwingUtilsTest {
+	/** Translator to use */
+	private Translator translator;
+	
+	/**
+	 * Set up test attributes.
+	 * @throws URISyntaxException
+	 *         if there is a problem with the path of the translation file.
+	 */
+	@Before
+	public void setUp () throws URISyntaxException {
+		translator = new Translator(Paths.get(getClass().getResource("/locale.xml").toURI()));
+	}
 	
 	/**
 	 * Test method for {@link com.alexrnl.commons.gui.swing.SwingUtils#setLookAndFeel(java.lang.String)}.
@@ -30,5 +49,23 @@ public class SwingUtilsTest {
 		}
 		UIManager.installLookAndFeel(new LookAndFeelInfo("LDR", "com.alexrnl.commons.gui.swing.ldr"));
 		assertFalse(SwingUtils.setLookAndFeel("LDR"));
+	}
+	
+	/**
+	 * Test method for {@link com.alexrnl.commons.gui.swing.SwingUtils#getMenu(com.alexrnl.commons.translation.Translator, String)}.
+	 */
+	@Test
+	public void testGetMenu () {
+		final JMenu open = SwingUtils.getMenu(translator, "commons.menu.file.open");
+		assertEquals("Open a file", open.getText());
+		assertEquals('O', open.getMnemonic());
+
+		final JMenu close = SwingUtils.getMenu(translator, "commons.menu.file.close");
+		assertEquals("Close a file", close.getText());
+		assertEquals('\0', close.getMnemonic());
+
+		final JMenu quit = SwingUtils.getMenu(translator, "commons.menu.file.quit");
+		assertEquals("Quit the application", quit.getText());
+		assertEquals('\0', quit.getMnemonic());
 	}
 }
