f93ac1c229104d53bb5f1f37b3a69591cf18600f
Alex Barféty <alexbarfety@free.fr>
2013-08-14 12:37:31 +0200

    Fix attribute alignment

--- src/main/java/com/alexrnl/commons/io/IOUtils.java
+++ src/main/java/com/alexrnl/commons/io/IOUtils.java
@@ -13,10 +13,10 @@ import java.util.logging.Logger;
  */
 public final class IOUtils {
 	/** Logger */
-	private static Logger	lg	= Logger.getLogger(IOUtils.class.getName());
+	private static Logger			lg						= Logger.getLogger(IOUtils.class.getName());
 	
 	/** The byte order mark used at the beginning of unicode files */
-	public static final Character UNICODE_BYTE_ORDER_MARK = '\ufeff';
+	public static final Character	UNICODE_BYTE_ORDER_MARK	= '\ufeff';
 	
 	/**
 	 * Constructor #1.<br />
