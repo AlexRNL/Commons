3d2b623d96880cc0983629682f25689ebd1d1a00
Alex Barféty <alexbarfety@free.fr>
2013-12-27 18:33:03 +0100

    Refactor the parse method
    Remove the boolean checking and parsing as well as the post-processing
    for the errors and help.

--- src/test/java/com/alexrnl/commons/arguments/ArgumentsTest.java
+++ src/test/java/com/alexrnl/commons/arguments/ArgumentsTest.java
@@ -3,6 +3,9 @@ package com.alexrnl.commons.arguments;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -103,6 +106,25 @@ public class ArgumentsTest {
 		assertTrue(target.isUsed());
 		assertEquals("manLau", target.getName());
 		assertTrue(target.isB());
+	}
+	
+	/**
+	 * Test method for checking help request.
+	 */
+	@Test
+	public void testHelpRequest () {
+		try {
+			arguments.parse("-h");
+		} catch (final IllegalArgumentException e) {
+			verify(out, times(1)).println(arguments);
+			verify(out, never()).println(anyString());
+		}
+		try {
+			arguments.parse("--help");
+		} catch (final IllegalArgumentException e) {
+			verify(out, times(1)).println(arguments);
+			verify(out, never()).println(anyString());
+		}
 	}
 	
 	/**
