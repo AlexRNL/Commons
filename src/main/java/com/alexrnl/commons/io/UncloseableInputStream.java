5d3a06d834a553c03b45171c290506dacb3fa2af
Alex Barféty <alexbarfety@free.fr>
2014-09-24 00:13:59 +0200

    Override all methods of InputStream

--- src/main/java/com/alexrnl/commons/io/UncloseableInputStream.java
+++ src/main/java/com/alexrnl/commons/io/UncloseableInputStream.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 /**
  * Decorator for an input stream that should not be closed.<br />
  * For example, use on <code>System.in</code>:
+ * 
  * <pre>
  * System.setIn(new UncloseableInputStream(System.in));
  * </pre>
@@ -28,6 +29,41 @@ public class UncloseableInputStream extends InputStream {
 	@Override
 	public int read () throws IOException {
 		return inputStream.read();
+	}
+	
+	@Override
+	public int read (final byte[] b) throws IOException {
+		return inputStream.read(b);
+	}
+	
+	@Override
+	public int read (final byte[] b, final int off, final int len) throws IOException {
+		return inputStream.read(b, off, len);
+	}
+	
+	@Override
+	public long skip (final long n) throws IOException {
+		return inputStream.skip(n);
+	}
+	
+	@Override
+	public int available () throws IOException {
+		return inputStream.available();
+	}
+	
+	@Override
+	public synchronized void mark (final int readlimit) {
+		inputStream.mark(readlimit);
+	}
+	
+	@Override
+	public synchronized void reset () throws IOException {
+		inputStream.reset();
+	}
+	
+	@Override
+	public boolean markSupported () {
+		return inputStream.markSupported();
 	}
 	
 	@Override
--- src/test/java/com/alexrnl/commons/io/UncloseableInputStreamTest.java
+++ src/test/java/com/alexrnl/commons/io/UncloseableInputStreamTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 /**
- * Test suite for the {@link UncloseableInputStream} class.
+ * TODO
  * @author Alex
  */
 public class UncloseableInputStreamTest {
@@ -51,5 +51,81 @@ public class UncloseableInputStreamTest {
 	public void testClose () throws IOException {
 		uncloseableStream.close();
 		verify(underlyingStream, never()).read();
+	}
+
+	
+	/**
+	 * Test method for {@link UncloseableInputStream#read(byte[])}.
+	 * @throws IOException
+	 *         if there was an I/O error.
+	 */
+	@Test
+	public void testReadByteArray () throws IOException {
+		final byte[] byteArray = new byte[8];
+		uncloseableStream.read(byteArray);
+		verify(underlyingStream).read(byteArray);
+	}
+	
+	/**
+	 * Test method for {@link UncloseableInputStream#read(byte[], int, int)}.
+	 * @throws IOException
+	 *         if there was an I/O error.
+	 */
+	@Test
+	public void testReadByteArrayIntInt () throws IOException {
+		final byte[] byteArray = new byte[8];
+		uncloseableStream.read(byteArray, 8, 28);
+		verify(underlyingStream).read(byteArray, 8, 28);
+	}
+	
+	/**
+	 * Test method for {@link UncloseableInputStream#skip(long)}.
+	 * @throws IOException
+	 *         if there was an I/O error.
+	 */
+	@Test
+	public void testSkip () throws IOException {
+		uncloseableStream.skip(88);
+		verify(underlyingStream).skip(88);
+	}
+	
+	/**
+	 * Test method for {@link UncloseableInputStream#available()}.
+	 * @throws IOException
+	 *         if there was an I/O error.
+	 */
+	@Test
+	public void testAvailable () throws IOException {
+		uncloseableStream.available();
+		verify(underlyingStream).available();
+	}
+	
+	/**
+	 * Test method for {@link UncloseableInputStream#mark(int)}.
+	 */
+	@Test
+	public void testMark () {
+		uncloseableStream.mark(48);
+		verify(underlyingStream).mark(48);
+	}
+	
+	/**
+	 * Test method for {@link UncloseableInputStream#reset()}.
+	 * @throws IOException
+	 *         if there was an I/O error.
+	 */
+	@Test
+	public void testReset () throws IOException {
+		uncloseableStream.reset();
+		verify(underlyingStream).reset();
+	}
+	
+	/**
+	 * Test method for {@link UncloseableInputStream#markSupported()}.
+	 */
+	@Test
+	public void testMarkSupported () {
+		uncloseableStream.markSupported();
+		verify(underlyingStream).markSupported();
 	}
 }
