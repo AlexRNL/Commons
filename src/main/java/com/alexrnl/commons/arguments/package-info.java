e762543b567624e5924f6d913d13a8a20343f5a6
Alex Barféty <alexbarfety@free.fr>
2015-11-29 19:57:13 +0100

    Improve Arguments package javadocs

--- src/main/java/com/alexrnl/commons/arguments/package-info.java
+++ src/main/java/com/alexrnl/commons/arguments/package-info.java
@@ -1,5 +1,8 @@
 /**
- * Package for command line parsing tools.
+ * Package for command line parsing tools.<br />
+ * The class {@link com.alexrnl.commons.arguments.Arguments} can be used to parse command line
+ * arguments. Fields that are set by the parsing must be annotated with the annotation
+ * {@link com.alexrnl.commons.arguments.Param}.
  * @author Alex
  */
 package com.alexrnl.commons.arguments;
